95cdb6f169c8198e00e4c2025c55b00e1fc26d86
jaroslawmichalik <michalik@protonmail.ch>
2017-11-23 19:04:01 +0100

    Addec customers

--- src/main/kotlin/com/michalik/api/MainController.kt
+++ src/main/kotlin/com/michalik/api/MainController.kt
@@ -18,8 +18,12 @@ val mockUsers = listOf<Customer>(
         Customer(UUID.randomUUID().toString(), "Jane", "Werberg", "werber@internet.com", "+48 402 423 555", "https://randomuser.me/api/portraits/women/${(Random().nextInt() % 99) * -1}.jpg"),
         Customer(UUID.randomUUID().toString(), "Erwin", "Schrodinger", "erwin@internet.com", "+48 222 333 444", "https://randomuser.me/api/portraits/men/${(Random().nextInt() % 99) * -1}.jpg"),
         Customer(UUID.randomUUID().toString(), "Max", "Plank", "max@internet.com", "+48 527 423 107", "https://randomuser.me/api/portraits/men/${(Random().nextInt() % 99) * -1}.jpg"),
-        Customer(UUID.randomUUID().toString(), "Marie", "Curie", "marie@internet.com", "+48 534 253 037", "https://randomuser.me/api/portraits/women/${(Random().nextInt() % 99) * -1}.jpg")
-)
+        Customer(UUID.randomUUID().toString(), "Franz", "Ferdynand", "franz@internet.com", "+48 434 253 037", "https://randomuser.me/api/portraits/men/${(Random().nextInt() % 99) * -1}.jpg"),
+        Customer(UUID.randomUUID().toString(), "Luna", "Lovegood", "luna@internet.com", "+48 534 853 037", "https://randomuser.me/api/portraits/women/${(Random().nextInt() % 99) * -1}.jpg"),
+        Customer(UUID.randomUUID().toString(), "Eva", "March", "eva@internet.com", "+48 534 253 037", "https://randomuser.me/api/portraits/women/${(Random().nextInt() % 99) * -1}.jpg"),
+        Customer(UUID.randomUUID().toString(), "Roman", "Pietrovsky", "roman@internet.com", "+48 524 253 037", "https://randomuser.me/api/portraits/men/${(Random().nextInt() % 99) * -1}.jpg"),
+        Customer(UUID.randomUUID().toString(), "Han", "Multiple", "han@internet.com", "+48 594 253 037", "https://randomuser.me/api/portraits/men/${(Random().nextInt() % 99) * -1}.jpg"),
+        Customer(UUID.randomUUID().toString(), "Tatiana", "Okrutnik", "okrutnik1@internet.com", "+48 534 253 037", "https://randomuser.me/api/portraits/women/${(Random().nextInt() % 99) * -1}.jpg"))
 
 @RestController
 @RequestMapping("/api")
